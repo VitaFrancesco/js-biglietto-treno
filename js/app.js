53aa1c3d5ebbfa599275857714eaf56af461e63a
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-14 15:50:45 +0200

    ADD: decimal rounding at price

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@ let km = parseInt(prompt("Insrisci i km che vorresti percorrere...", 10))
 const age = parseInt(prompt("Inserisci la tua età..."))
 
 // prezzo iniziale dato dai km
-let prezzo = km * 0.21
+let price = km * 0.21
 
 // sconto dato dall'età
 let sconto = 0
 if (age < 18) {
-    sconto = prezzo * 20 / 100
+    discount = price * 20 / 100
 } else if (age >= 65) {
-    sconto = prezzo * 40 / 100
+    discount = price * 40 / 100
 }
 
 // prezzo finale senza arrotondamento al secondo decimale
-prezzo = 'Il prezzo del biglietto è ' + (prezzo - sconto) + '€'
-console.log(prezzo)
+price = price - discount
+console.log('Il prezzo del biglietto è ' + price.toFixed(2) + '€')

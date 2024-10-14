031c008f1fe129fa966d02b12d7ff2f7d3771881
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-14 16:21:49 +0200

    FEAT: discount calculation

--- js/app.js
+++ js/app.js
@@ -6,13 +6,14 @@ const age = parseInt(prompt("Inserisci la tua età..."))
 let price = km * 0.21
 
 // sconto dato dall'età
-let sconto = 0
-if (age < 18) {
-    discount = price * 20 / 100
-} else if (age >= 65) {
-    discount = price * 40 / 100
-}
+// let discount = 0
+// if (age < 18) {
+//     discount = price * 20 / 100
+// } else if (age >= 65) {
+//     discount = price * 40 / 100
+// }
+
+let discount = age < 18 ? price * 20 /100 : (age >= 65 ? price * 40 /100: 0)
 
 // prezzo finale senza arrotondamento al secondo decimale
-price = price - discount
-console.log('Il prezzo del biglietto è ' + price.toFixed(2) + '€')
+console.log('Il prezzo del biglietto è ' + (price-discount).toFixed(2) + '€')

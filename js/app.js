afdbc5f236bf95d03f38bcd103ea9c69b4b69ab6
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-14 17:26:43 +0200

    ADD: problematic control age value

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 // richiesta valori in input
 let km = parseInt(prompt("Insrisci i km che vorresti percorrere...", 10))
-const age = parseInt(prompt("Inserisci la tua età..."))
+let age
+
+// controllo dei valori ricevuti
+let controlAge = false
+while (controlAge === false) {
+    age = prompt("Inserisci la tua età...")
+    if ((!(age < 100) && !(age > 0)) || age !== Number) {
+        alert("L'età deve essere espressa in numeri interi e compresa tra 0 e 100!")
+    } else {
+        controlAge = true
+    }
+}
+while (!(age < 100) && !(age > 0) && (typeof(age) === Number)) {
+    alert("L'età deve essere espressa in numeri interi e compresa tra 0 e 100!")
+    age = prompt("Inserisci la tua età...")
+}
+age = parseInt(age)
 
 // prezzo iniziale dato dai km
 let price = km * 0.21
@@ -13,6 +29,7 @@ let price = km * 0.21
 //     discount = price * 40 / 100
 // }
 
+// sconto dato dall'età con operatore ternario
 let discount = age < 18 ? price * 20 /100 : (age >= 65 ? price * 40 /100: 0)
 
 // prezzo finale senza arrotondamento al secondo decimale
